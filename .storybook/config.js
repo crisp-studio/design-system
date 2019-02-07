1bc4b9c0aaf21032c2f7174e120fff44c9f5d7b0
Daniel Wirtz <danielwirtzx@gmail.com>
2019-02-07 22:16:01 +0100

    :sparkles: Add storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
-import { configure, setAddon } from "@storybook/react";
-import JSXAddon from "storybook-addon-jsx";
+import { addDecorator, configure, setAddon } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { withKnobs } from "@storybook/addon-knobs/react";
 
-setAddon(JSXAddon);
+addDecorator(withInfo({ header: true, inline: true }));
+addDecorator(withKnobs);
+
 const req = require.context("../src", true, /.stories.jsx$/);
 
 function loadStories() {
